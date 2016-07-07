c7ccb370415246b08450aac232f4cfb1f9cfd01c
Brian Han <brianhan@tstartel.com>
2016-07-07 09:27:11 +0800

    Initial Commit

---
+++ Puss/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Puss
+//
+//  Created by Brian Han on 2016/7/7.
+//  Copyright © 2016年 Brian Han. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Puss/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Puss
+//
+//  Created by Brian Han on 2016/7/7.
+//  Copyright © 2016年 Brian Han. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
